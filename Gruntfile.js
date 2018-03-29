291d262c7101310f1fc9cb141cea7fa3f474f536
Dan Sirbu <dan.infinitylabz@gmail.com>
2018-03-29 12:29:44 +0300

    Added build folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,152 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks("grunt-contrib-less");
+    /*grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-browser-sync");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-postcss");*/
+
+    require("load-grunt-tasks")(grunt);
 
     grunt.initConfig({
+        clean: {
+            build: ["build"]
+        },
+
+        copy: {
+            build: {
+                files: [{
+                    expand: true,
+                    src: [
+                        "fonts/**/*.{woff,woff2}",
+                        "img/**",
+                        "js/**",
+                        "*.html"
+                    ],
+                    dest: "build"
+                }]
+            },
+            html: {
+                files: [{
+                    expand: true,
+                    src: ["*.html"],
+                    dest: "build"
+                }]
+            }
+        },
+
         less: {
             style: {
-                files:{
-                    "css/style.css": "less/style.less"
-
+                files: {
+                    "build/css/style.css": "less/style.less"
                 }
 
             }
 
-        }
+        },
 
+        postcss: {
+            style: {
+                options: {
+                    processors: [
+                        require("autoprefixer")({
+                            browsers:
+                                [
+                                    "last 1 version",
+                                    "last 2 Chrome versions",
+                                    "last 2 Firefox versions",
+                                    "last 2 Opera versions",
+                                    "last 2 Edge versions"
+                                ]
+                        }),
+                        require("css-mqpacker")({
+                            sort: true
+                        })
+                    ]
+
+                },
+                src: "build/css/*.css"
+            }
+        },
+
+        csso: {
+            style: {
+                options: {
+                    report: "gzip"
+                },
+                files: {
+                    "build/css/style.min.css": ["build/css/style.css"]
+                }
+            }
+        },
+
+        svgstore: {
+            options: {
+                svg: {
+                    style: "display: none"
+                }
+            },
+            symbols: {
+                files: {
+                    "build/img/symbols.svg": ["img/icons/*.svg"]
+                }
+            }
+        },
+
+        svgmin: {
+            symbols: {
+                files: [{
+                    expand: true,
+                    src: ["build/img/icons/*.svg"]
+                }]
+            }
+        },
+
+        imagemin: {
+            images: {
+                options: {
+                    optimizationLevel: 3
+                },
+                files: [{
+                    expand: true,
+                    src: ["build/img/**/*.{png,jpg,gif}"]
+                }]
+            }
+        },
+
+        browserSync: {
+            server: {
+                bsFiles: {
+                    src: ["build/*.html", "build/css/*.css"]
+                },
+                options: {
+                    server: "build",
+                    watchTask: true
+                }
+            }
+        },
+
+        watch: {
+            html: {
+                files: ["*.html"],
+                task: ["copy:html"]
+            },
+            style: {
+                files: ["less/**/*.less"],
+                tasks: ["less", "postcss", "csso"]
+            }
+        }
     });
+
+    grunt.registerTask("serve", ["browserSync", "watch"]);
+    grunt.registerTask("symbols", ["svgmin", "svgstore"]);
+
+    grunt.registerTask("build", [
+        "clean",
+        "copy",
+        "less",
+        "postcss",
+        "csso",
+        "symbols",
+        "imagemin"
+    ]);
 
 };
